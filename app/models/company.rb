c8b4b2b03cbe4607f507b87399bf4c1b2d8bb75f
a <chitresh.hsharma@gmail.com>
2020-07-20 17:05:57 +0530

    Added Custom Validation to company email Address (When Present)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
-
+  EMAIL_REGEX = /\A[\w+\.]+@getmainstreet.com\z/i
+  validates :email, allow_blank: true,
+                    format: { with: EMAIL_REGEX, message: "should have domain '@getmainstreet.com'" }
 end
